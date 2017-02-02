099e162752b5b55f8496e3d088d07fae4800e0e0
Ethan Bennett <ebennett19@gmail.com>
2017-02-02 15:12:50 -0700

    Setup files to accomodate specified endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "home#index"
   get "/auth/github", as: :github_login
   get "/auth/github/callback", to: "sessions#create"
+  resources :users, only: [:show]
 end
