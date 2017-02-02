5b89dca07c2efa5acb603e33487a93b6deb1d698
Ethan Bennett <ebennett19@gmail.com>
2017-02-02 15:16:39 -0700

    Add infrastructure for api calls

---
+++ app/services/github_service.rb
@@ -0,0 +1,18 @@
+class GithubService
+
+  def self.account_info(uid)
+  end
+
+  def self.recent_activity(uid)
+  end
+
+  def self.following(uid)
+  end
+
+  def self.organizations(uid)
+  end
+
+  def self.repositories(uid)
+  end
+
+end
