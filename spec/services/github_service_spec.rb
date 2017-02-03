9d20d06512b6fea0a33c0b9c75c0a852a6f36c90
Ethan Bennett <ebennett19@gmail.com>
2017-02-02 18:16:44 -0700

    Add repositories test

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -5,8 +5,7 @@ describe GithubService do
     it "returns relevant account information" do
       VCR.use_cassette("github_service_account_info") do
         account_info = GithubService.account_info("ethanbennett")
-        # Don't test for specific data
-        # Specific keys are a good idea
+
         expect(account_info).to be_a(Hash)
         expect(account_info.count).to eq(30)
         expect(account_info).to have_key(:login)
@@ -16,6 +15,24 @@ describe GithubService do
         expect(account_info).to have_key(:email)
         expect(account_info).to have_key(:hireable)
         expect(account_info).to have_key(:bio)
+      end
+    end
+  end
+
+  context ".repositories" do
+    it "returns a list of all user repositories" do
+      VCR.use_cassette("github_service_repositories") do
+        repositories = GithubService.repositories("ethanbennett")
+        
+        expect(repositories).to be_an(Array)
+        repository = repositories.first
+
+        expect(repositories.first[:owner][:login]).to eq("ethanbennett")
+        expect(repository).to have_key(:id)
+        expect(repository).to have_key(:name)
+        expect(repository).to have_key(:full_name)
+        expect(repository).to have_key(:owner)
+        expect(repository).to have_key(:commits_url)
       end
     end
   end
